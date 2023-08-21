0662a36ae0f6d48ef0385c75e8e4f102bd893b8a
Roiben7 <roibenshoham@gmail.com>
2023-08-21 18:13:42 +0300

    fixing bug

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -20,8 +20,8 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Roi",
+  email: "Roi@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
